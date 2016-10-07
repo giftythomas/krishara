cf1029ed3764329dd117ef665159dff7f7f526d3
MyProject <Ramakrishna@RKLaptop>
2016-10-07 16:53:58 +0530

    Some minor changes

--- FreeStyle/src/main/java/com/freestyle/controller/CartController.java
+++ FreeStyle/src/main/java/com/freestyle/controller/CartController.java
@@ -7,14 +7,11 @@ import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.freestyle.model.Cart;
@@ -39,8 +36,7 @@ public class CartController {
 	UserService userService;
 	@Autowired
 	CartService cartService;
-	@RequestMapping(value="/userCart/{product_id}",method=RequestMethod.PUT)
-	@ResponseStatus(value=HttpStatus.NO_CONTENT)
+	@RequestMapping(value="/userCart")
 	public String addToCart(@RequestParam(value="product_id") int product_id, @AuthenticationPrincipal org.springframework.security.core.userdetails.User activeUser){
 		User user=userService.getUserByName(activeUser.getUsername());
 		Cart cart=user.getCart();
--- FreeStyle/src/main/java/com/freestyle/model/User.java
+++ FreeStyle/src/main/java/com/freestyle/model/User.java
@@ -11,7 +11,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Size;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.springframework.stereotype.Component;
